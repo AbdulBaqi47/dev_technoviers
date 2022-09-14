691ce8c857a38d09b0c2f3160ecabf3746c3366a
Your Name <you@example.com>
2022-09-15 00:08:42 +0500

    welcome to hibaq technovier

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="block">
+                    <a href="#"><img src="{{url('img/logo.png')}}"  height="60" width="225"  alt=""></a>
+                    <p>All rights reserved © 2015</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+
+<!-- load Js -->
+<script src="{{url('js/jquery-1.11.3.min.js')}}"></script>
+<script src="{{url('js/bootstrap.min.js')}}"></script>
+<script src="{{url('http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&amp;sensor=false')}}">
+</script>
+<script src="{{url('js/waypoints.min.js')}}"></script>
+<script src="{{url('js/lightbox.js')}}"></script>
+<script src="{{url('js/jquery.counterup.min.js')}}"></script>
+<script src="{{url('js/owl.carousel.min.js')}}"></script>
+<script src="{{url('js/html5lightbox.js')}}"></script>
+<script src="{{url('js/jquery.mixitup.js')}}"></script>
+<script src="{{url('js/wow.min.js')}}"></script>
+<script src="{{url('js/jquery.scrollUp.js')}}"></script>
+<script src="{{url('js/jquery.sticky.js')}}"></script>
+<script src="{{url('js/jquery.nav.js')}}"></script>
+<script src="{{url('js/main.js')}}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>AL-HIBAQ Technovier</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--Favicon-->
+	<link rel="icon" href="{{url('img/titleicon.png')}}" type="image/jpg" />
+	<!-- google fonts -->
+
+	<!-- Css link -->
+	<link rel="stylesheet" href="{{url('http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
+	<link rel="stylesheet" href="{{url('css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{url('css/owl.carousel.css')}}">
+	<link rel="stylesheet" href="{{url('/owl.transitions.css')}}">
+	<link rel="stylesheet" href="{{url('css/animate.min.css')}}">
+	<link rel="stylesheet" href="{{url('css/lightbox.css')}}">
+	<link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{url('css/preloader.css')}}">
+	<link rel="stylesheet" href="{{url('css/image.css')}}">
+	<link rel="stylesheet" href="{{url('css/icon.css')}}">
+	<link rel="stylesheet" href="{{url('css/style.css')}}">
+	<link rel="stylesheet" href="{{url('css/responsive.css')}}">
+
+</head>
+
+<body id="top">
+	<header id="navigation" class="navbar-fixed-top animated-header">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- responsive nav button -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<!-- /responsive nav button -->
+
+				<!-- logo -->
+				<h1 class="navbar-brand">
+					<a href="#body"><img src="img/logo.png" height="60" width="164" alt=""></a>
+				</h1>
+				<!-- /logo -->
+			</div>
+
+			<!-- main nav -->
+			<nav class="collapse navbar-collapse navbar-right" role="navigation">
+				<ul id="nav" class="nav navbar-nav menu">
+					<li><a href="#top">Home</a></li>
+					<li><a href="#features">Service</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#team">Team</a></li>
+					<li><a href="#pricing-table">Price</a></li>
+					<li><a href="#blog">Blog</a></li>
+					<li><a href="#testimonial">Testimonial</a></li>
+					<li><a href="#contact-form">Contact</a></li>
+				</ul>
+			</nav>
+			<!-- /main nav -->
+
+		</div>
+	</header>
